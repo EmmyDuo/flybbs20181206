cbc6ebe789e1814b449f0e25c74d55ec65d87416
zxj <604798605@qq.com>
2018-12-19 14:46:38 +0800

    帖子分类

--- src/main/java/com/neusoft/controller/JieController.java
+++ src/main/java/com/neusoft/controller/JieController.java
@@ -1,14 +1,18 @@
 package com.neusoft.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.neusoft.domain.Comment;
 import com.neusoft.domain.Topic;
 import com.neusoft.domain.TopicCategory;
 import com.neusoft.domain.User;
+import com.neusoft.mapper.CommentMapper;
 import com.neusoft.mapper.TopicCategoryMapper;
 import com.neusoft.mapper.TopicMapper;
 import com.neusoft.respones.RegRespObj;
+import com.neusoft.util.StringDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,7 +20,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("jie")
@@ -26,6 +33,15 @@ public class JieController {
     TopicCategoryMapper topicCategoryMapper;
     @Autowired
     TopicMapper topicMapper;
+    @Autowired
+    CommentMapper commentMapper;
+    @RequestMapping("index/{cid}")
+    public ModelAndView index(@PathVariable Integer cid){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jie/index");
+        modelAndView.addObject("cid",cid);
+        return modelAndView;
+    }
     @RequestMapping("add")
     public ModelAndView jie(){
         List<TopicCategory> topicCategoryList = topicCategoryMapper.getAllCategories();
@@ -40,13 +56,56 @@ public class JieController {
         HttpSession httpSession = request.getSession();
         User user = (User)httpSession.getAttribute("userinfo");
         topic.setUserid(user.getId());
+        topic.setCreateTime(new Date());
+        Integer kissNum = topic.getKissNum();
+
         int result = topicMapper.insertSelective(topic);
-        if(result > 0)
-        {
+        if(result > 0){
             regRespObj.setStatus(0);
             regRespObj.setAction(request.getServletContext().getContextPath() + "/");
             response.getWriter().println(JSON.toJSONString(regRespObj));
         }
+        return;
+    }
+    @RequestMapping("detail/{tid}")
+    public ModelAndView detail(@PathVariable Integer tid) {
+        ModelAndView modelAndView = new ModelAndView();
+        Map<String,Object> map = topicMapper.getTopicInfo(tid);
+        Date date = (Date)map.get("create_time");
+        String strDate = StringDate.getStringDate(date);
+        map.put("create_time",strDate);
+        List<Map<String,Object>> mapList = commentMapper.getCommentsByTopicID(tid);
+        for(Map<String,Object> map2 : mapList) {
+            Date date2 = (Date)map2.get("comment_time");
+            String strDate2 = StringDate.getStringDate(date2);
+            map2.put("comment_time",strDate2);
+        }
+        modelAndView.setViewName("jie/detail");
+        modelAndView.addObject("topic",map);
+        modelAndView.addObject("comments",mapList);
+        return modelAndView;
+    }
+    @RequestMapping("reply")
+    public void reply(Comment comment, String content, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        RegRespObj regRespObj = new RegRespObj();
+        HttpSession httpSession = request.getSession();
+        Object object = httpSession.getAttribute("userinfo");
+        User user = (User)httpSession.getAttribute("userinfo");
+        if(user != null) {
+            comment.setCommentContent(content);
+            comment.setUserId(user.getId());
+            comment.setCommentTime(new Date());
+            commentMapper.insertSelective(comment);
+            regRespObj.setStatus(0);
+            Topic topic = topicMapper.selectByPrimaryKey(comment.getTopicId());
+            int comment_num = topic.getCommentNum();
+            topic.setCommentNum(comment_num + 1);
+            topicMapper.updateByPrimaryKeySelective(topic);
+        }else {
+            regRespObj.setStatus(0);
+            regRespObj.setAction(request.getServletContext().getContextPath()+"/user/login");
+        }
+        response.getWriter().println(JSON.toJSONString(regRespObj));
         return;
     }
 }
